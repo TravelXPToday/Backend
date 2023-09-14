11246ef47b77a3226134d5444301a85450fad91a
artofghost <67277022+Artnooijen@users.noreply.github.com>
2023-09-14 13:58:43 +0200

    added connected the api to our DB

--- API/routes.py
+++ API/routes.py
@@ -1,9 +1,12 @@
 from flask import Blueprint, jsonify
-from controllers import get_journey
-
+from controllers import get_journey, get_all_travelers
 
 bp = Blueprint('routes', __name__)
 
 @bp.route('/journey', methods=['GET'])
 def journey():
     return jsonify(get_journey())
+
+@bp.route('/traveler/all', methods=['GET'])
+def travelers():
+    return jsonify(get_all_travelers())

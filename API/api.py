de50dccb234992d5f351c84a3600d4d9876c21c8
artofghost <67277022+Artnooijen@users.noreply.github.com>
2023-09-14 12:27:04 +0200

    added functionality and routes  to api

--- API/api.py
+++ API/api.py
@@ -1,17 +1,26 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 from routes import bp as routes_bp
 import re
 from datetime import date
-import datetime
-
+import Utils.config as config
 from flask import Flask, render_template, request
 from pymongo import MongoClient
 
 
-app = Flask(__name__)
-app.register_blueprint(routes_bp)
+server = Flask(__name__)
+CORS(server)
+server.register_blueprint(routes_bp)
+
+@server.route('/traveler', methods=['GET'])
+def get_traveler():
+    return jsonify(get_traveler()), 200
+
+@server.route('/journey', methods=['GET'])
+def get_journey():
+    return jsonify(get_journey()), 200
 
 if __name__ == "__main__":
-    app.run()
+    server.run(host=config.HOST, port=config.PORT)
 
-client = MongoClient('YOUR_CONNECTION_STRING')
+client = MongoClient(config.CONN)

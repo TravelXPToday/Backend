14ceec3e53e08475b23e66b2faf8966c89a957aa
JManders07 <113422379+JManders07@users.noreply.github.com>
2023-12-14 10:42:47 +0100

    Update traveler.py

--- API/traveler.py
+++ API/traveler.py
@@ -4,5 +4,4 @@ from crud_interface import CRUDInterface
 
 class Traveler(DataAccess, CRUDInterface):
     def __init__(self):
-        print("traveler self: " + str(self))
         super().__init__('traveler')
